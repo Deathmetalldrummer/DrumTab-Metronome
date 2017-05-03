8a9cd77478b51a72fba519dffa4cc498c219be8b
Kirill Terehin <DeathMetallDrummer@gmail.com>
2017-05-03 20:49:36 +0600

    go home

--- src/app/metronome/metronome.component.ts
+++ src/app/metronome/metronome.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from '../app.service';
 
 @Component({
   selector: 'app-metronome',
   templateUrl: './metronome.component.html',
   styleUrls: ['./metronome.component.css']
 })
-export class MetronomeComponent {
+export class MetronomeComponent implements OnInit {
   title = 'Metronome';
+  constructor(private appService: AppService) {
+  }
+  play: any;
+  onPlay() {
+    this.play = setTimeout(() => {
+      const audio = new Audio();
+      audio.src = '../../assets/sound/snare.mp3';
+      audio.autoplay = true;
+    }, 0);
+
+    }
+  onStop() {
+    clearInterval(this.play);
+  }
+  ngOnInit() {
+  }
+
 }

ffc8fb43c3e004dcecfdb8019d6d94a0b05b16da
Ragnarok <DeathMetallDrummer@gmail.com>
2017-05-04 10:12:30 +0600

    Циклирует аудио

--- src/app/metronome/metronome.component.ts
+++ src/app/metronome/metronome.component.ts
@@ -8,19 +8,32 @@ import { AppService } from '../app.service';
 })
 export class MetronomeComponent implements OnInit {
   title = 'Metronome';
+  play: any;
   constructor(private appService: AppService) {
   }
-  play: any;
   onPlay() {
-    this.play = setTimeout(() => {
-      const audio = new Audio();
-      audio.src = '../../assets/sound/snare.mp3';
-      audio.autoplay = true;
-    }, 0);
+    for (let j = 0; j < this.appService.data.length; j++ ) {
+
+      let i = 0;
+      this.play = setInterval((): void => {
+        if (this.appService.data[j][i] === 1) {
+          const audio = new Audio();
+          audio.src = '../../assets/sound/bass.mp3';
+          audio.autoplay = true;
+        }
+        i++;
+        console.log(this.play.runCount);
+        if (i === this.appService.data[j].length) {
+          i = 0;
+        }
+      }, 300);
+
+
+
+    }
 
     }
   onStop() {
-    clearInterval(this.play);
   }
   ngOnInit() {
   }
